28d4df65f4afc3e2209ea2be6844517d9ae4d448
SofyCastillo <sofiatrinidadcastillo@gmail.com>
2024-04-11 23:29:14 -0300

    iniciamos proyecto

---
+++ Seguro-2024/src/main/java/P3G2Seguro4/IMB/Seguro4/Seguro2024Application.java
@@ -0,0 +1,13 @@
+package P3G2Seguro4.IMB.Seguro4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seguro2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seguro2024Application.class, args);
+	}
+
+}
---
+++ Seguro-2024/src/test/java/P3G2Seguro4/IMB/Seguro4/Seguro2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package P3G2Seguro4.IMB.Seguro4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seguro2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
