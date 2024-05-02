d5cfa7c48eb3cd6ffbf468b964d4a9cd39c32b26
Mario Ahumada <ahumadamob@gmail.com>
2024-05-02 17:45:06 -0300

    Se blanquea la rama main

--- src/main/java/imb/pr2/club/repository/DisciplinaRepository.java
+++
@@ -1,9 +0,0 @@
-package imb.pr2.club.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import imb.pr2.club.entity.Disciplina;
-
-public interface DisciplinaRepository extends JpaRepository<Disciplina, Integer>{
-
-}
